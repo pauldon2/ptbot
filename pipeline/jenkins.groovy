fcb8d6eabc4ac61d5623a04938408a0fa1408fda
Pavlo.Shevtsov <paulshevtsov@gmail.com>
2024-04-19 17:36:58 +0300

    Add Jenkins pipeline

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    environment {
+        REPO = 'https://github.com/pauldon2/ptbot'
+        BRANCH = 'develop'
+        GITHUB = credentials('github')
+        TARGETARCH = "${params.ARCH}"
+        TARGETOS = "${params.OS}"
+    }
+
+    stages {
+        stage('clone') {
+            steps {
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Building image started"
+                sh "printenv"
+                sh "make image"
+            }
+        }
+
+        stage('login to GHCR') {
+            steps {
+                sh "echo $GITHUB_PSW | docker login ghcr.io -u $GITHUB_USR --password-stdin"
+            }
+        }
+        
+        stage('push image') {
+            steps {
+              sh "make push"
+            }
+        } 
+    }
+}
